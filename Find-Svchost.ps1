ca190c346aecb804eda65ca306996f727292f87e
jacobjessup <jacobwjessup@gmail.com>
2022-01-25 15:36:25 -0500

    typo change

--- Find-Svchost.ps1
+++ Find-Svchost.ps1
@@ -1,2 +1,2 @@
-Get-WmiObject win32_service | where {$_.Started -eq "True" -and $).ServiceType -eq "Share Process"} | select Name, ProcessID | Sort-Object ProcessId
+Get-WmiObject win32_service | where {$_.Started -eq "True" -and $_.ServiceType -eq "Share Process"} | select Name, ProcessID | Sort-Object ProcessId
 
